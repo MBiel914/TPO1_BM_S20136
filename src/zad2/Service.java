ac8206e4192531ec2c34946d913ae160002d74a3
s20136 <s20136@pjwstk.edu.pl>
2022-03-13 13:55:39 +0100

    Send API request ready and read response using JSONObject

--- src/zad2/Service.java
+++ src/zad2/Service.java
@@ -7,14 +7,21 @@
 package zad2;
 
 import java.io.StringWriter;
-import org.json.simple.JSONObject;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import org.json.JSONObject;
 
 public class Service {
 	private static String _countryName;
 	private static String _cityName;
 	private static String _currencyAcronym;
 
-	private static String host = "api.openweathermap.org/data/2.5/weather";
+	private static String host = "http://api.openweathermap.org/data/2.5/weather";
 	// Headers for a request
 	private static String x_rapidapi_host = "movie-database-imdb-alternative.p.rapidapi.com";
 	private static String openweathermap_key = "8560aca6dea319ae8a3a0b8dbdd2464b";
@@ -41,18 +48,19 @@ public class Service {
 
 	public static String getWeather(String cityName) {
 		try {
-			JSONObject obj = new JSONObject();
+			String apiUrl = host  + "?" + "q=London,uk" + "&appid=" + openweathermap_key;
+			System.out.println(apiUrl.toString());
+			HttpClient client = HttpClient.newHttpClient();
+		    HttpRequest request = HttpRequest.newBuilder()
+		          .uri(URI.create(apiUrl))
+		          .build();
 
-			obj.put("name", "foo");
-			obj.put("num", new Integer(100));
-			obj.put("balance", new Double(1000.21));
-			obj.put("is_vip", new Boolean(true));
-
-			StringWriter out = new StringWriter();
-			obj.writeJSONString(out);
-
-			String jsonText = out.toString();
-			System.out.print(jsonText);
+		    HttpResponse<String> response =
+		          client.send(request, BodyHandlers.ofString());
+		    JSONObject jsonObject = new JSONObject(response.body());
+		    
+		    System.out.println(jsonObject.toString());
+		    System.out.println(jsonObject.getJSONObject("main").get("humidity").toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
